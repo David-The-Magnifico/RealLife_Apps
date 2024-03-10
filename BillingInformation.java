8ac70944ead1a87282b1e6cf712718ee97d451a0
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-10 11:51:08 +0100

    Create BillingInformation.java

---
+++ BillingInformation.java
@@ -0,0 +1,5 @@
+public class BillingInformation {
+    private String receiverPhone;
+    private String receiverName;
+    private String deliveryAddress;
+    private CreditCardInformation creditCardInformation;
