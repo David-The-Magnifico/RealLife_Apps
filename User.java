b97245e9d39a4e3c75c01b7824f7501ee35f8119
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-10 11:33:02 +0100

    User.java
    commit on MAR 10,2024

---
+++ User.java
@@ -0,0 +1,60 @@
+import java.util.List;
+
+// User class
+public class User {
+    private int age;
+    private String email;
+    private String homeAddress;
+    private String name;
+    private String password;
+    private String phone;
+
+    // Getters and Setters
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
