c8c5c346cc60c04f2d72a68123b74613c9373fbb
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-10 12:21:04 +0100

    commit on MAR 10,2024

---
+++ Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    private int productId;
+    private String productName;
+    private double price;
+    private String productDescription;
+    private ProductCategory category;
+
+
+}
