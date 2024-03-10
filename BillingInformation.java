78a2422343811cc1e5fec05d8bfd4858323a727c
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-10 11:57:28 +0100

    BillingInformation.java

--- BillingInformation.java
+++ BillingInformation.java
@@ -3,3 +3,9 @@ public class BillingInformation {
     private String receiverName;
     private String deliveryAddress;
     private CreditCardInformation creditCardInformation;
+
+    public String getReceiverPhone() {
+        return receiverPhone;
+    }
+
+    
