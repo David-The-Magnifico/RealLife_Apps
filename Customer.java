b26bdb681b489d121aac2ae0c793b0ce158a8953
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-10 11:40:29 +0100

    Customer.java

--- Customer.java
+++ Customer.java
@@ -1,3 +1,7 @@
 public class Customer extends User {
     private List<BillingInformation> billingInformationList;
     private ShoppingCart shoppingCart;
+
+    public void addBillingInformation(BillingInformation billingInformation) {
+        billingInformationList.add(billingInformation);
+    }
