2d79302fde84a888ab31d2a265d1ac2882e0cf44
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-10 12:34:21 +0100

    commit on MAR 10, 2024

---
+++ CreditCardInformation.java
@@ -0,0 +1,9 @@
+public class CreditCardInformation {
+    private String cardCvv;
+    private int cardExpirationYear;
+    private int cardExpirationMonth;
+    private String creditCardNumber;
+    private String nameOnCard;
+    private CardType cardType;
+
+}
