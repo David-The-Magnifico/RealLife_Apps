0dd9ba086e977f76060db0acbd925f84fdcab908
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-17 17:26:07 +0100

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -6,4 +6,8 @@ public final class Admin extends User {
     private List<Customer> customers = new ArrayList<>();
     private int numberOfCustomers;
 
+    public Admin(int id, String name, int age, String emailAddress, Address homeAddress, String password, String phone) {
+        super(id, name, age, emailAddress, homeAddress, password, phone);
+    }
+
     
