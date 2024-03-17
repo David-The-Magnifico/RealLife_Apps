39d6d5e20c70dae63e5dd4cd34da1d5c67e30a8b
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-17 17:25:16 +0100

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -2,3 +2,8 @@ import oopEstore.exceptions.IncorrectPasswordException;
 import java.util.ArrayList;
 import java.util.List;
 
+public final class Admin extends User {
+    private List<Customer> customers = new ArrayList<>();
+    private int numberOfCustomers;
+
+    
