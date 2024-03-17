2a704d11bd3fe4e82d8f9d424de595f8810874c3
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-17 17:30:36 +0100

    Admin.java

--- Admin.java
+++ Admin.java
@@ -32,4 +32,35 @@ public final class Admin extends User {
         customers.remove(customer);
     }
 
-    
+    private static void validate(Customer customer, String password) {
+        if (!customer.isCorrect(password)) throw new IncorrectPasswordException("Password provided is not correct");
+    }
+
+    public Customer findCustomer(int customerId) {
+        for (Customer customer : customers) if (customer.getId() == customerId) return customer;
+        throw new IllegalArgumentException("Customer with id " + customerId + " not found");
+    }
+
+    public List<Product> getProducts() {
+        return Products.view();
+    }
+
+    public void addInitialInventory() {
+        Product electronic = new Product(Products.generateProductId(), "LG", 6_700_000.0, "Smart TV",
+                ProductCategory.ELECTRONICS);
+        Product grocery = new Product(Products.generateProductId(), "Bokku", 1_100.0, "Bread", ProductCategory.GROCERIES);
+        Product utensil = new Product(Products.generateProductId(), "Cutlery", 1_000.0, "Silverset", ProductCategory.UTENSILS);
+        Product clothing = new Product(Products.generateProductId(), "Ralph Lauren", 20_000.0, "Polo Shirt", ProductCategory.CLOTHING);
+
+        Products.add(electronic, grocery, utensil, clothing);
+    }
+
+    public Product findProductById(int productId) {
+        return Products.findProductById(productId);
+    }
+}
+
+
+
+
+
